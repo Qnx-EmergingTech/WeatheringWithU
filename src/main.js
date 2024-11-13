fb2a53b7062f958700356a90d79d4b44434737ec
John Edward Complido <john.edward_complido@questronix.com.ph>
2024-11-13 12:40:20 +0800

    [FRONTEND] Import Font Awesome Icons for weather condition indicator

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCloud, faSun, faCloudRain } from '@fortawesome/free-solid-svg-icons';
 
+library.add(faCloud, faSun, faCloudRain);
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+
+app.component('font-awesome-icon', FontAwesomeIcon);
